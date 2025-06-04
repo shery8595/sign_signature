dddeec24616cf98ca49f9a41b5c784daecec3e4f
shery8595 <131074050+shery8595@users.noreply.github.com>
2025-06-04 14:33:55 +0500

    Create firebase-config.js

---
+++ api/firebase-config.js
@@ -0,0 +1,10 @@
+// /api/firebase-config.js
+export const firebaseConfig = {
+  apiKey: "AIzaSyADd3GsMnoZuoltCFFXQWms4bzEVTNVn3U",
+  authDomain: "signature-app-878a8.firebaseapp.com",
+  projectId: "signature-app-878a8",
+  storageBucket: "signature-app-878a8.appspot.com",
+  messagingSenderId: "524854956900",
+  appId: "1:524854956900:web:e926d9ea12f44af124a071",
+  measurementId: "G-8SCTEYWL17"
+};
